297c4e48480efc15909792bfa47c07eb4b67593a
learnershakil <learnershakil@gmail.com>
2024-08-12 02:30:19 +0530

    Practise-1: fix minor issue

--- react/colorChanger/src/App.jsx
+++ react/colorChanger/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <button className='bg-yellow-500 rounded-lg px-2 py-1' onClick={()=> setColor('yellow')}>yelloe</button>
           <button className='bg-pink-500 rounded-lg px-2 py-1' onClick={()=> setColor('pink')}>pink</button>
           <button className='bg-purple-500 rounded-lg px-2 py-1' onClick={()=> setColor('purple')}>purple</button>
-          <button className='bg-slate-500 rounded-lg px-2 py-1' onClick={()=> setColor('slate')}>slate</button>
+          <button className='bg-slate-500 rounded-lg px-2 py-1' onClick={()=> setColor('gray')}>slate</button>
         </div>
       </div>
      </div>
