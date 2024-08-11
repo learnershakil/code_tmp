32b41a82d220d5fcb82dfd63909d3ed4bdaf2ff8
learnershakil <learnershakil@gmail.com>
2024-08-11 19:16:33 +0530

    setup tailwind css

--- react/Revision/src/App.jsx
+++ react/Revision/src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Counter: {count}</h1>
-        <button onClick={() => {if(count<20){setCount((count) => count + 1)}}}>
+    <div className="text-4xl font-bold underline text-red-600 ml-96"> <h1>
+      Hello world!
+    </h1></div>
+   
+      <h1 className='border-slate-700 border border-solid rounded-2xl px-2'>Counter: {count}</h1>
+        <button className='border-slate-700 border border-solid rounded-2xl px-2' onClick={() => {if(count<20){setCount((count) => count + 1)}}}>
           Increase
         </button>
         <br />
-        <br />
-        <button onClick={() => {if(count > 0){setCount((count) => count - 1)}}}>
+        <button className='border-slate-700 border border-solid rounded-2xl px-2' onClick={() => {if(count > 0){setCount((count) => count - 1)}}}>
           Decrease
         </button>
     </>
