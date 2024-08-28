31ff4b04531ab1d8fd2a43c88e79784935944906
learnershakil <learnershakil@gmail.com>
2024-08-29 04:56:55 +0530

    Mern Project: 'Shivam ko sign in route ka logic samjha diya!!'

--- paytm/backend/routes/user.js
+++ paytm/backend/routes/user.js
@@ -13,8 +13,13 @@ const userSchema = zod.object({
   lastName: zod.string(),
 });
 
-router.get("/", (req, res) => {
-  res.send("Welcome to user route!!");
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.post("/signup", async (req, res) => {
@@ -25,8 +30,8 @@ router.post("/signup", async (req, res) => {
 
   try {
     const userExist = await User.findOne({
-        username: req.body.username
-    })
+      username: req.body.username,
+    });
 
     if (userExist) {
       return res.status(400).json({ error: "User already exists!!" });
@@ -45,6 +50,45 @@ router.post("/signup", async (req, res) => {
     res.status(201).json({
       message: "User created successfully!!",
       token: token,
+    });
+  } catch (error) {
+    res.status(500).json({ error: `Internal server error, ${error}` });
+  }
+});
+
+const signinBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+
+router.post("/signin", async (req, res) => {
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+
+  try {
+    const userExist = await User.findOne({
+      username: req.body.username,
+    });
+
+    if (!userExist) {
+      return res.status(400).json({ error: "User not found!!" });
+    } else if (userExist.password !== req.body.password) {
+      return res.status(400).json({ error: "Invalid password!!" });
+    } else {
+      const userId = userExist._id;
+      const token = jwt.sign({ userId }, JWT_SECRET);
+      res.status(200).json({
+        message: "User signed in successfully!!",
+        token: token,
+      });
+    }
+
+    res.status(411).json({
+      message: "Error while logging in",
     });
   } catch (error) {
     res.status(500).json({ error: `Internal server error, ${error}` });
