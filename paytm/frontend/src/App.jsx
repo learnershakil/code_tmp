ad15178da5d3fea565c6d41ca333e78262c5a433
learnershakil <learnershakil@gmail.com>
2024-09-07 16:29:19 +0530

    Paytm: I clear her doubt early, so i delete paytm basic project code

--- paytm/frontend/src/App.jsx
+++ paytm/frontend/src/App.jsx
@@ -1,9 +1,11 @@
+
 function App() {
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center text-red-500">Hello world!</h1>{" "}
+        Hello world
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
